c5642599efc3ed631df5c4458b79ee0f4eaac184
ChrisW0524 <chriswang20050524@gmail.com>
2021-04-26 14:02:46 -0500

    Added user input

--- main.py
+++ main.py
@@ -11,25 +11,32 @@ mainClock = pygame.time.Clock()
 mainScreen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("3D Graphics Engine")
 
-# Initialize matrix variables
+# Initialize matrix and camera variables
 scale = 100
+changeScale = 0
 
+# Camera translation
 locX = 0
 locY = 0
 locZ = 0
 
-rotX = 0
-rotY = 0
-rotZ = 0
+# Camera rotation
+rotX, rotY, rotZ = 0, 0, 0
+changeRotX, changeRotY, changeRotZ = 0, 0, 0
+
+
 
 # Draw method ---------------------------------------------------------------------------------------------------------#
 def draw(points):
     mainScreen.fill((255, 255, 255))
     for point in points:
-        pygame.draw.circle(mainScreen,(0, 0, 0), (point[0, 0] + WINDOW_SIZE[0] / 2, point[1, 0]+ WINDOW_SIZE[1] / 2),10)
+        pygame.draw.circle(mainScreen, (0, 0, 0), (point[0, 0] + WINDOW_SIZE[0] / 2, point[1, 0] + WINDOW_SIZE[1] / 2),
+                           10)
+        for i in range(8):
+            draw_lines(point, points[i])
 
     # Draw lines
-    draw_lines(points[0], points[1])
+    '''draw_lines(points[0], points[1])
     draw_lines(points[1], points[3])
     draw_lines(points[2], points[3])
     draw_lines(points[2], points[0])
@@ -40,15 +47,16 @@ def draw(points):
     draw_lines(points[0], points[4])
     draw_lines(points[1], points[5])
     draw_lines(points[3], points[7])
-    draw_lines(points[2], points[6])
+    draw_lines(points[2], points[6])'''
 
     pygame.display.update()
     mainClock.tick(FPS)
 
+
 # draw lines connecting points
 def draw_lines(a, b):
     pygame.draw.line(mainScreen, (0, 0, 0), (a[0, 0] + WINDOW_SIZE[0] / 2, a[1, 0] + WINDOW_SIZE[1] / 2),
-                                                (b[0, 0] + WINDOW_SIZE[0] / 2, b[1, 0] + WINDOW_SIZE[1] / 2), 2)
+                     (b[0, 0] + WINDOW_SIZE[0] / 2, b[1, 0] + WINDOW_SIZE[1] / 2), 2)
 
 
 # Matrix calculations -------------------------------------------------------------------------------------------------#
@@ -76,22 +84,32 @@ def update(points):
                           [0, 0, 0, 1]])
 
     for point in points:
-        updated_point = np.matmul(rotationX, point)
+        updated_point = np.matmul(displacement, point)
+        updated_point = np.matmul(rotationX, updated_point)
         updated_point = np.matmul(rotationY, updated_point)
         updated_point = np.matmul(rotationZ, updated_point)
-        updated_point = np.matmul(displacement, updated_point)
         updated_points.append(updated_point)
 
     return updated_points
 
 
+def update_variables():
+    global scale, changeScale
+    global rotX, rotY, rotZ
+    global changeRotX, changeRotY, changeRotZ
+    rotX += changeRotX
+    rotY += changeRotY
+    rotZ += changeRotZ
+    scale += changeScale
+
+
 # Main game loop ------------------------------------------------------------------------------------------------------#
 def main():
     # Initialize game loop variables
-    global rotX
-    global rotY
-    global rotZ
-    global scale
+    global rotX, rotY, rotZ
+    global changeRotX, changeRotY, changeRotZ
+    global locX, locY, locZ
+    global scale, changeScale
     run = True
 
     # Initialize points ([[x], [y], [z], [1]])
@@ -107,10 +125,11 @@ def main():
 
     while run:
         # Update
-        rotX += math.pi / 360
+        '''rotX += math.pi / 360
         rotY += math.pi / 360
-        rotZ += math.pi / 360
-        scale += 1
+        rotZ += math.pi / 360'''
+        update_variables()
+
         projected_points = update(points)
 
         # Draw
@@ -118,9 +137,35 @@ def main():
 
         # Event loop
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                continue
 
+            #KEYDOWN
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    changeRotX = math.pi / 360
+                if event.key == pygame.K_DOWN:
+                    changeRotX = -math.pi / 360
+                if event.key == pygame.K_LEFT:
+                    changeRotY = math.pi / 360
+                if event.key == pygame.K_RIGHT:
+                    changeRotY = -math.pi / 360
+                if event.key == pygame.K_z:
+                    changeRotZ = math.pi / 360
+                if event.key == pygame.K_x:
+                    changeRotZ = -math.pi / 360
+                if event.key == pygame.K_n:
+                    changeScale = -1
+                if event.key == pygame.K_m:
+                    changeScale = 1
+            #KEYUP
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    changeRotX = 0
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    changeRotY = 0
+                if event.key == pygame.K_z or event.key == pygame.K_x:
+                    changeRotZ = 0
+                if event.key == pygame.K_n or event.key == pygame.K_m:
+                    changeScale = 0
             # Exit code
             if event.type == pygame.QUIT:
                 run = False
